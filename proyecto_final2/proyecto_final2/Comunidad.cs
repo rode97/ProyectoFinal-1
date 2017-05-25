da271541bf5990c7828a363f751ba398aff5b84e
Roderico <r_eduardodlcc97@hotmail.com>
2017-05-25 09:47:12 -0600

    primera clase

---
+++ proyecto_final2/proyecto_final2/Comunidad.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace proyecto_final2
+{
+    public class Comunidad
+    {
+        private string nombre;
+        private string codigo;
+    }
+}
