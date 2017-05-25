d41b9ee18a72542000495b451d48c0a18f6adb84
Roderico <r_eduardodlcc97@hotmail.com>
2017-05-25 09:43:52 -0600

    todo

---
+++ proyecto_final2/proyecto_final2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(proyecto_final2.Startup))]
+namespace proyecto_final2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
